95d8e8d3047402f9c47d77a029ad3fd84886da60
Maame <mbonamensa@gmail.com>
2022-07-28 15:50:51 +0000

    add updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@
     el.addEventListener('scroll', listener)
   }
 
-  /**
+   /**
    * Navbar links active state on scroll
    */
   let navbarlinks = select('#navbar .scrollto', true)
@@ -53,6 +53,7 @@
   }
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
+
 
   /**
    * Scrolls to an element with header offset
@@ -170,77 +171,4 @@
   }
 
 })()
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active-dot", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active-dot";
-}
-
-
-function plusSlides(position) {
-    slideIndex += position;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    else if(slideIndex < 1){slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active-dot", "");
-
-      }
-        slides[slideIndex-1].style.display = "block";  
-        dots[slideIndex-1].className += " active-dot";
-    }
-
-function currentSlide(index) {
-    if (index > slides.length) {index = 1}
-    else if(index < 1){index = slides.length}
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active-dot", "");
-      }
-        slides[index-1].style.display = "block";  
-        dots[index-1].className += " active-dot";
-    }
-
-function showSlides() {
-    var i;
-    slides = document.getElementsByClassName("mySlides");
-    dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active-dot", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active-dot";
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
-}
 
